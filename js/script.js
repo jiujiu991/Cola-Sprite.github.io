20d4948cc7100a19be77f990b9671c8ad1142e82
Cheng qian <751470926@qq.com>
2024-04-08 15:37:14 +0800

    Site updated: 2024-04-08 15:37:13

--- js/script.js
+++ js/script.js
@@ -1,28 +1,8 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+// Calculate the number of days since the start date
+var startDate = new Date("Sep 29, 2022 00:00:00").getTime();
+var now = new Date().getTime();
+var distance = now - startDate;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-
-var x = setInterval(function() {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-    // If the count down is over, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-}, 1000);
+// Update the inner HTML of the 'demo' element
+document.getElementById("demo").innerHTML = "我们在一起 " + days + " 天 <span class='love'>&#10084;</span>";
